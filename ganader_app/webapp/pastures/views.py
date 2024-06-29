f1671240d2aefaf8e6004fb7b6d482c8f4c242aa
ramirososa <53145455+ramirososa@users.noreply.github.com>
2024-06-29 14:58:43 -0300

    App pasturas

--- ganader_app/webapp/pastures/views.py
+++ ganader_app/webapp/pastures/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Pasture
+from .models import Pasture, Ranch
 from .forms import PastureForm
 
 # Create your views here.
 
 def pasture_list(request, ranch_id):
     ranch = get_object_or_404(Ranch, id=ranch_id)
-    pastures = Pasture.objects.filter(ranch=ranch)
-    return render(request, 'pastures/pasture_list.html', {'pastures': pastures})
+    pastures = Pasture.objects.filter(ranch_id=ranch_id)
+    return render(request, 'pastures/pasture_list.html', {'pastures': pastures, 'ranch': ranch})
 
 def add_pasture(request, ranch_id):
     ranch = get_object_or_404(Ranch, id=ranch_id)
@@ -17,7 +17,7 @@ def add_pasture(request, ranch_id):
             pasture = form.save(commit=False)
             pasture.ranch = ranch
             pasture.save()
-            return redirect('pasture_list', ranch_id=ranch.id)
+            return redirect('pasture-list', ranch_id=ranch.id)
     else:
         form = PastureForm()
     return render(request, 'pastures/add_pasture.html', {'form': form, 'ranch': ranch})
