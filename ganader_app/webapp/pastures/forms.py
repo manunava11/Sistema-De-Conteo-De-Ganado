be889798b5d8fc0d2f1919ed163eaed78366ca7f
ramirososa <53145455+ramirososa@users.noreply.github.com>
2024-06-30 18:30:46 -0300

    version control before migrating to bootsrap 5

--- ganader_app/webapp/pastures/forms.py
+++ ganader_app/webapp/pastures/forms.py
@@ -1,7 +1,18 @@
 from django import forms
 from .models import Pasture
+from lots.models import Lot
 
 class PastureForm(forms.ModelForm):
     class Meta:
         model = Pasture
         fields = ['name', 'area']
+
+class LotSelectionForm(forms.Form):
+    #lot_id = forms.ModelChoiceField(queryset=Lot.objects.filter(pasture__isnull=True), label="Available Lots")
+    #lot_id = forms.ModelChoiceField(queryset=Lot.objects.all(), to_field_name='id', label='Select Lot')
+    #lot_id = forms.ModelChoiceField(queryset=Lot.objects.filter(pasture__isnull=True), to_field_name='id', label='Select Lot')
+    lot_id = forms.ModelChoiceField(
+        queryset=Lot.objects.filter(pasture__isnull=True),
+        to_field_name='id',
+        label='Select Lot'
+    )
--- ganader_app/webapp/pastures/views.py
+++ ganader_app/webapp/pastures/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Pasture, Ranch
-from .forms import PastureForm
-
-# Create your views here.
+from lots.models import Lot
+from .forms import PastureForm, LotSelectionForm
 
 def pasture_list(request, ranch_id):
     ranch = get_object_or_404(Ranch, id=ranch_id)
     pastures = Pasture.objects.filter(ranch_id=ranch_id)
-    return render(request, 'pastures/pasture_list.html', {'pastures': pastures, 'ranch': ranch})
+    available_lots = Lot.objects.filter(ranch_id=ranch_id, pasture__isnull=True)
+
+    if request.method == 'POST':
+        form = LotSelectionForm(request.POST)
+        if form.is_valid():
+            lot_id = form.cleaned_data['lot_id'].id
+            pasture_id = request.POST.get('pasture_id')
+            lot = get_object_or_404(Lot, id=lot_id)
+            lot.pasture_id = pasture_id
+            lot.save()
+            return redirect('pasture-list', ranch_id=ranch_id)
+    else:
+        form = LotSelectionForm()
+
+    return render(request, 'pastures/pasture_list.html', {'pastures': pastures, 'form': form, 'available_lots': available_lots, 'ranch': ranch})
 
 def add_pasture(request, ranch_id):
     ranch = get_object_or_404(Ranch, id=ranch_id)
