5373bf5e2660bde3ebf350b95a96cfb169648f1a
ramirososa <53145455+ramirososa@users.noreply.github.com>
2024-07-07 17:51:12 -0300

    Add No lot option to pasture

--- ganader_app/webapp/pastures/forms.py
+++ ganader_app/webapp/pastures/forms.py
@@ -8,4 +8,9 @@ class PastureForm(forms.ModelForm):
         fields = ['name', 'area']
 
 class LotSelectionForm(forms.Form):
-    lot_id = forms.ModelChoiceField(queryset=Lot.objects.all(), label='Select Lot')
+    lot_id = forms.ModelChoiceField(
+        queryset=Lot.objects.all(),
+        label='Select Lot',
+        required=False,
+        empty_label='No Lot'  # Add an empty option
+    )
